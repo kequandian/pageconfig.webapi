599b3b9ab4b4a074a00bc46c5cadef96bc7451cd
vincent@local.com <vincent@local.com>
2026-01-10 23:38:14 +0800

    reuse common-mongo-db

--- src/repositories/mongo_repo.rs
+++ src/repositories/mongo_repo.rs
@@ -1,46 +1,25 @@
-use mongodb::{Client, Collection, Database};
+use common_mongo_db::{JsonRepository, MongoDb};
+use std::sync::Arc;
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct MongoPageConfig {
-    #[serde(rename = "_id")]
-    pub page_id: String,
-    pub config: Value, // Store as raw JSON value for flexibility
-    pub updated_at: chrono::DateTime<chrono::Utc>,
-}
-
 pub struct MongoRepository {
-    collection: Collection<MongoPageConfig>,
+    repo: Arc<JsonRepository>,
 }
 
 impl MongoRepository {
     pub async fn new(uri: &str, db_name: &str) -> Result<Self> {
-        let client = Client::with_uri_str(uri).await?;
-        let db = client.database(db_name);
-        let collection = db.collection::<MongoPageConfig>("page_configs");
-        Ok(Self { collection })
+        let mongo = MongoDb::new(uri, db_name).await?;
+        // 集合名 "pageconfigs"，数据字段 "config"
+        let repo = Arc::new(JsonRepository::new(mongo, "pageconfigs", "config"));
+        Ok(Self { repo })
     }
 
     pub async fn get_config(&self, page_id: &str) -> Result<Option<Value>> {
-        let filter = mongodb::bson::doc! { "_id": page_id };
-        let result = self.collection.find_one(filter, None).await?;
-        Ok(result.map(|doc| doc.config))
+        self.repo.get(page_id).await.map_err(|e| anyhow::anyhow!(e))
     }
 
     pub async fn save_config(&self, page_id: &str, config: Value) -> Result<()> {
-        let options = mongodb::options::UpdateOptions::builder()
-            .upsert(true)
-            .build();
-        let query = mongodb::bson::doc! { "_id": page_id };
-        let update = mongodb::bson::doc! {
-            "$set": {
-                "config": mongodb::bson::to_bson(&config)?,
-                "updated_at": mongodb::bson::DateTime::now(),
-            }
-        };
-        self.collection.update_one(query, update, options).await?;
-        Ok(())
+        self.repo.upsert(page_id.to_string(), config).await.map_err(|e| anyhow::anyhow!(e))
     }
 }
